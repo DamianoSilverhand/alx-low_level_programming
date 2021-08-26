a1796db55f50804ec29c05dcbc74acaad2f577fd
Damiano Chintala <35993666+DamianoSilverhand@users.noreply.github.com>
2021-08-26 18:30:53 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *s = and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 long l = 59;
 long fd = 1;
 long syscall = 1;
