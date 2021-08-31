7feee990d7a3e6abf0b81aa2f4c115b178f18993
Damiano Chintala <35993666+DamianoSilverhand@users.noreply.github.com>
2021-08-31 12:07:29 +0200

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -3,8 +3,17 @@
 *main - prints Holberton
 *Return: 0
 */
-int main(void){
-char text[] = "Holberton";
-  printf("%s\n", text);
-  return 0;
- }
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
