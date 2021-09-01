3184f8a75e69f92fb94990045069b9e117b3c464
Damiano Chintala <damianochintala@gmail.com>
2021-09-01 17:26:52 +0200

    updated holberton header file

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif /* MAIN_H */
+#endif /* HOLBERTON_H */
