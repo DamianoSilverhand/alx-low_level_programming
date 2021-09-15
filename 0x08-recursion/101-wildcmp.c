4be93e3cc70cda3c8884878c5c921b6430f8e9c5
Damiano Chintala <damianochintala@gmail.com>
2021-09-15 16:20:27 +0200

    Wildcard comparison

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "mainn.h"
+/**
+ * wildcmp - compare two strings
+ * @s1: char
+ * @s2: char
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	else if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+
+	else if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	}
+
+return (0);
+}
